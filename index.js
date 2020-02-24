a116a44e22a139b7e7ae4ce743d7c6f4d909c780
WinGrigoreva <kriviktoria@gmail.com>
2020-02-24 22:16:42 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// lesson 2 - begin:
 // let express = require("express");
 // let app = express();
 // app.get("/", function (request, response){
-// response.send(request.query);
+// response.send("Hello!");
 // })
 // app.listen(591);
 
+//lesson 2 - continue:
 let wss = require("ws").Server;
 let server = new wss({port:591});
 let clients = new Set();
 server.on("connection", function(socket){
     clients.add(socket);
     socket.on("message", function(message){
+        //lesson 4 - begin
+        // console.log(message);
+        // if (message == "TestConnect") return;
+        //lesson 4 - end
         for (let i of clients){
             i.send(message);
         }
@@ -19,3 +25,12 @@ server.on("connection", function(socket){
         clients.delete(socket);
     });
 });
+
+
+let express = require("express");
+let app = express();
+app.use(express.static(__dirname));
+app.get("/", function (request, response){
+response.sendFile('index.html');
+})
+app.listen(80);
--- script.js
+++ script.js
@@ -1,15 +1,74 @@
-let field = $("#field"),
-    chat = $("#chat"),
-    ws = new WebSocket("ws://localhost:591");
-    ws.onmessage = function(message){
-        chat.val(message.data + "\n" + chat.val());        
-     };
+//lesson 1:
+let text     = $("#text"),
+    chat     = $("#chat"),
+    chat_body= $("#chat_body"),
+    name     = $("#name"),
+    chatname = $("#chatname"), //lesson 3
+    message  = $(".message"),
+    ws;
+    
+//lesson 3 - begin 
+chatname.change(function (event) {
+    if (ws) {
+        ws.close();
+        ws = null;
+        // message.remove();
+    }
+    //ws = new WebSocket("ws://192.168.1.44:591"); 
+    ws = new WebSocket(chatname.val()),
+//lesson 3 - end
 
-     ws.onopen = function() {
-         field.keydown(function(event){
-             if(event.which===13){
-                ws.send(field.val());
-                field.val("");
-             }
-         });
-     };
+    ws.onmessage = function (message_ws) {
+        let message_in = JSON.parse(message_ws.data);
+        let message_field = `<div class="message alert alert-light ${message_in.name == name.val() ? "message-right" : "message-left"}">
+                                <div class="username">${message_in.name}</div>
+                                <div class="usertext">${message_in.text}</div>
+                            </div>`;
+        chat.append(message_field);
+        chat_body.scrollTop(chat.height());
+
+    };
+
+    ws.onopen = function () {
+        text.keyup(function (event) {
+            if (event.which === 13 && name.val() && text.val()) {
+                let message_out = {
+                    name: name.val(),
+                    text: text.val(),
+                }
+                ws.send(JSON.stringify(message_out));
+                text.val("");
+            }
+        });
+    };
+//lesson 3 begin
+})
+    //lesson 4 begin:
+// chatname.find("option").toArray().forEach(element => {
+//     let el = $(element),
+//         loc_obj_synh = {isClose: false};
+//         ws1 = new WebSocket(el.val());
+//     ws1.onopen = function() {
+//         ws1.send("TestConnect");
+//         ws1.close();
+//         console.log("onopen");
+//         loc_obj_synh.isClose = true;
+//     };
+//     el.prop("style","color: green");
+//     ws1.onerror = function(){
+//         el.prop("style","color: red");
+//         ws1.close();
+
+//         console.log("onerror");
+//         loc_obj_synh.isClose = true;
+//     }
+//     ws1.onclose = function() {
+//         loc_obj_synh.isClose = true;
+//         console.log("onclose");
+//     }
+//     while (!loc_obj_synh.isClose) {
+//     }
+// });
+    //lesson 4 end:
+chatname.trigger("change");
+//lesson 3 end
